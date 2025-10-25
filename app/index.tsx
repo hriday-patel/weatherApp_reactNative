c592d692123d05495e421a92b1047de602a249a4
hriday-patel <patelhriday23@gmail.com>
2025-10-25 23:37:29 +0530

    not working

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,132 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Weather from "./Weather";
-import UpcomingWeather from "./UpcomingWeather";
-import City from "./City";
+import { WEATHER_API_KEY } from "@env";
 import Feather from "@expo/vector-icons/Feather";
-import { SafeAreaView } from "react-native-safe-area-context";
-import * as Location from 'expo-location';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import City from "./City";
+import UpcomingWeather from "./UpcomingWeather";
+import Weather from "./Weather";
+
+const Tab = createBottomTabNavigator();
 
 export default function Index() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
+  const getWeather = async (latitude: number, longitude: number) => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`
+      );
 
+      if (!res.ok) {
+        throw new Error("Failed to fetch weather data");
+      }
 
-  const Tab = createBottomTabNavigator();
+      const data = await res.json();
+      setWeatherData(data);
+    } catch (error) {
+      setErrorMsg("Failed to fetch weather data");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        setLoading(true);
+
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission Denied!");
+          setLoading(false);
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+
+        await getWeather(latitude, longitude);
+      } catch (error) {
+        setErrorMsg("Error getting location");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  if (loading) {
+    return (
+      <View className="flex-1 bg-white justify-center items-center">
+        <ActivityIndicator color={"purple"} size={"large"} />
+      </View>
+    );
+  }
+
+  if (errorMsg) {
+    return (
+      <View className="flex-1 bg-white justify-center items-center">
+        <Text className="text-red-500 text-lg">{errorMsg}</Text>
+      </View>
+    );
+  }
+
+  if (weatherData) {
+    console.log(weatherData);
+  }
+
   return (
-      <Tab.Navigator screenOptions={{
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'grey',
-        headerShown: false
-      }}>
-          <Tab.Screen name="Weather" component={Weather} options={{
-            tabBarIcon({focused}) {
-                return <Feather name="droplet" size={24} color={focused ? 'tomato': 'black'} />
-            },
-          }}/>
-          <Tab.Screen name="Upcoming" component={UpcomingWeather} options={{
-            tabBarIcon({focused}) {
-                return <Feather name="clock" size={24} color={focused ? 'tomato': 'black'} />
-            },
-          }}/>
-          <Tab.Screen name="City" component={City} options={{
-            tabBarIcon({focused}) {
-                return <Feather name="home" size={24} color={focused ? 'tomato': 'black'} />
-            },
-          }}/>
-      </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "grey",
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Weather"
+        component={Weather}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="droplet"
+              size={24}
+              color={focused ? "tomato" : "grey"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Upcoming"
+        component={UpcomingWeather}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="clock"
+              size={24}
+              color={focused ? "tomato" : "grey"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="City"
+        component={City}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="home"
+              size={24}
+              color={focused ? "tomato" : "grey"}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
-                              
